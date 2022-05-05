916c61f114ac66ebdd06d9f7ae83f2c231ff1afe
Henrique Balardin <balardin@gmail.com>
2022-05-05 07:01:34 -0300

    feat (create feedbacks): create route

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.post('/feedbacks', (request, response) => {
+  const { type, message, screenshot } = request.body;
+
+  response.status(201).send();
+});
+
 app.listen(3333, () => console.log('HTTP server is running! 🚀'));
